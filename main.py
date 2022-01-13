310e0a476dbf0bd41b007310dbbba8519a95f181
HARUN PEHLİVAN <trcmnhp@hotmail.com>
2022-01-13 18:06:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+import matplotlib.pyplot as plt
+from matplotlib.path import Path
+from matplotlib.patches import PathPatch
+
+N = 400
+t = np.linspace(0, 2 * np.pi, N)
+r = 0.5 + np.cos(t)
+x, y = r * np.cos(t), r * np.sin(t)
+
+fig, ax = plt.subplots()
+ax.plot(x, y, "k")
+ax.set(aspect=1)
